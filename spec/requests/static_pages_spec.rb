3ed70aef94d15ad000204d8af6f7424e2b2af984
tkmiya34 <tkmiya34@gmail.com>
2014-12-14 22:42:21 +0900

    Add home, help, abount as static page
    静的ページとしてhome, help, aboutページを追加
    Rspecでのテストコードも

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,10 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe "StaticPages", :type => :request do
-  describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
-      get static_pages_index_path
-      expect(response).to have_http_status(200)
+  # describe "GET /static_pages" do
+  #   it "works! (now write some real specs)" do
+  #     get static_pages_index_path
+  #     expect(response).to have_http_status(200)
+  #   end
+  # end
+
+  describe "Home page" do
+    it "should have the content 'Sample App" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
+  end
+
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+  end
+
+  describe "Abount page" do
+    it "should have the content 'Abount Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('Abount Us')
     end
   end
 end
