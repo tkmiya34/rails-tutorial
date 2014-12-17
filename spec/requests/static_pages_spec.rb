86da904ac66e09b305ba115a1018eee49b9f6c80
tkmiya34 <tkmiya34@gmail.com>
2014-12-17 16:59:18 +0900

    Add contact as static pages
    静的ページにコンタクトページを追加
    テストコードの重複をリファクタリング

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,46 +1,61 @@
 require 'rails_helper'
 
-RSpec.describe "StaticPages", :type => :request do
-  # describe "GET /static_pages" do
-  #   it "works! (now write some real specs)" do
-  #     get static_pages_index_path
-  #     expect(response).to have_http_status(200)
-  #   end
-  # end
+RSpec.describe 'StaticPages', type: :request do
+  let(:base_title) { 'Ruby on Rails Tutorial Sample App' }
 
   describe 'Home page' do
+    let(:page_url) { '/static_pages/home' }
+
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit page_url
       expect(page).to have_content('Sample App')
     end
 
     it 'should have the right title' do
-      visit '/static_pages/home'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
+      visit page_url
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
 
   describe 'Help page' do
+    let(:page_url) { '/static_pages/help' }
+
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit page_url
       expect(page).to have_content('Help')
     end
 
     it 'should have the right title' do
-      visit '/static_pages/help'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+      visit page_url
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
   describe 'Abount page' do
+    let(:page_url) { '/static_pages/about' }
+
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
     end
 
     it 'should have the right title' do
-      visit '/static_pages/about'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | About Us')
+      visit page_url
+      expect(page).to have_title("#{base_title} | About Us")
+    end
+  end
+
+  describe 'Contact page' do
+    let(:page_url) { '/static_pages/contact' }
+
+    it "should have the content 'Contact'" do
+      visit page_url
+      expect(page).to have_content('Contact')
+    end
+
+    it 'should have the right title' do
+      visit page_url
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
 end
