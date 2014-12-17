313fae7454d1108ddf0ec0b9925fe09284849c08
tkmiya34 <tkmiya34@gmail.com>
2014-12-17 14:17:04 +0900

    Edit titile of static_pages
    静的ページのタイトルを動的に出し分けるように変更

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,24 +8,39 @@ RSpec.describe "StaticPages", :type => :request do
   #   end
   # end
 
-  describe "Home page" do
-    it "should have the content 'Sample App" do
+  describe 'Home page' do
+    it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+
+    it 'should have the right title' do
+      visit '/static_pages/home'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
+    end
   end
 
-  describe "Help page" do
+  describe 'Help page' do
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
+
+    it 'should have the right title' do
+      visit '/static_pages/help'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+    end
   end
 
-  describe "Abount page" do
-    it "should have the content 'Abount Us'" do
+  describe 'Abount page' do
+    it "should have the content 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_content('Abount Us')
+      expect(page).to have_content('About Us')
+    end
+
+    it 'should have the right title' do
+      visit '/static_pages/about'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | About Us')
     end
   end
 end
