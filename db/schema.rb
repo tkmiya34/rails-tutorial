58e5fdb9710898827cf92387ded2b8a6e0f1644d
TakanobuMiyazawa <tkmiya34@gmail.com>
2015-01-05 21:02:54 +0900

    Set DB migration. User table add index
    ユーザテーブルのemailカラムにインデックスを追加する

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150102093109) do
+ActiveRecord::Schema.define(version: 20150103134325) do
 
   create_table "users", force: true do |t|
     t.string   "name"
