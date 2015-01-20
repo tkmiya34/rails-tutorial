ca09ad2af0857ba49b98b6424d856837e58ad0a3
TakanobuMiyazawa <tkmiya34@gmail.com>
2015-01-21 00:08:51 +0900

    Add profile page

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe 'UserPages', type: :request do
+RSpec.describe 'User Pages', type: :request do
   subject { page }
+
+  describe 'profile page' do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
+  end
 
   describe 'signup page' do
     before { visit signup_path }
