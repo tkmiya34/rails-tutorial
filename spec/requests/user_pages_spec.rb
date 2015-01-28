7bf2aa6a5f6019aeefb04d599a78eaced858cf5c
TakanobuMiyazawa <tkmiya34@gmail.com>
2015-01-28 23:25:10 +0900

    Add page for users list

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,6 +3,23 @@ require 'rails_helper'
 RSpec.describe 'User Pages', type: :request do
   subject { page }
 
+  describe 'index' do
+    before do
+      sign_in FactoryGirl.create(:user)
+      FactoryGirl.create(:user, name: "Bob", email: "bob@example.com")
+      FactoryGirl.create(:user, name: "Jon", email: "jon@example.com")
+      visit users_path
+    end
+
+    it { should have_title('All users') }
+    it { should have_content('All users') }
+    it "should list each user" do
+      User.all.each do |user|
+        expect(page).to have_selector('li', text: user.name)
+      end
+    end
+  end
+
   describe 'profile page' do
     let(:user) { FactoryGirl.create(:user) }
     before { visit user_path(user) }
