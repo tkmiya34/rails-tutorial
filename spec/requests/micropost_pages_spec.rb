a17e89733bf88012247265c8f223c5cad50f9868
TakanobuMiyazawa <tkmiya34@gmail.com>
2015-02-03 18:00:50 +0900

    Add feature to create micropost

---
+++ spec/requests/micropost_pages_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+RSpec.describe "MicropostPages", type: :request do
+  subject { page }
+
+  let(:user) { FactoryGirl.create(:user) }
+
+  before { sign_in user }
+
+  describe "micropost creation" do
+    before { visit root_path }
+
+    describe "with invalid infomation" do
+      it "should not create a mimcropost" do
+        expect { click_button "Post" }.not_to change(Micropost, :count)
+      end
+
+      describe "error messages" do
+        before { click_button "Post" }
+        it { should have_content("error") }
+      end
+    end
+
+    describe "with valid infomation" do
+      before { fill_in "micropost_content", with: "Lorem ipsum" }
+      it "should create a micropost" do
+        expect { click_button "Post" }.to change(Micropost, :count).by(1)
+      end
+    end
+  end
+end
